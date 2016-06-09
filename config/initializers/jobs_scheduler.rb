3dd8b32b2e977747bf6ba892afc875d740a7ab5d
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-06-10 00:43:27 +0300

    error catching

--- config/initializers/jobs_scheduler.rb
+++ config/initializers/jobs_scheduler.rb
@@ -8,7 +8,13 @@ scheduler.every "2s" do
 		jobs.each do |job|
 			job.acknowledge!
 			attributes = job.message.attributes
-			JobRunner.new( attributes )			
+			begin
+				JobRunner.new( attributes )			
+			rescue => e
+				logger.info "app_log #{e.message} ---- #{e.backtrace}"
+      	$stderr.puts "app_log #{e.message} ---- #{e.backtrace}"
+      	$stdout.puts "app_log #{e.message} ---- #{e.backtrace}"
+			end
 		end
 	end
 
