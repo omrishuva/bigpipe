bd4777b8d57faff65e6c1c31e2f977cd43f5a982
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-06-10 09:36:16 +0300

    logger

--- app/jobs/job_runner.rb
+++ app/jobs/job_runner.rb
@@ -8,7 +8,7 @@ class JobRunner
 	end
 	
 	def send_password_recovery_email
-		Rails.logger.info AppMailer.password_recovery_code_mail( args["email"], args["name"], args["password_recovery_code"] ).deliver_now
+		AppMailer.password_recovery_code_mail( args["email"], args["name"], args["password_recovery_code"] ).deliver_now
 	end
 	
 
