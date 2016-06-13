da23cfcdbd41599459c407bc34bb437a202e8134
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-06-13 20:16:38 +0300

    more logs

--- app/jobs/send_password_recovery_email.rb
+++ app/jobs/send_password_recovery_email.rb
@@ -4,11 +4,15 @@ class SendPasswordRecoveryEmail < ActiveJob::Base
   def perform( args )
   	Rails.logger.info 
   	begin
+  		Rails.logger.info "Sending Email"
 	  	AppMailer.password_recovery_code_mail( args["email"], args["name"], args["password_recovery_code"] ).deliver_now
-	  	Rails.logger.info "Send Successfully"
+	  	Rails.logger.info "Sent Successfully"
 		rescue => e
+			Rails.logger.info "==================================="
 			Rails.logger.info "#{e.message}"
+			Rails.logger.info "-----------------------------------"
 			Rails.logger.info "#{e.backrace}"
+			Rails.logger.info "==================================="
 		end
 	end
 
