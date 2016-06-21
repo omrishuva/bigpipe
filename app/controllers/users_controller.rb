6a20a2c113a8b3f7761f796ce1e489458a649a0b
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-06-21 16:41:57 +0300

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -104,18 +104,11 @@ class UsersController < ApplicationController
   end
   
   def fb_lead
-    begin
-      user_params = params[:Parameters].merge!( password: User.generate_password, phone_verified: true )
-      user =  User.new( user_params )
-      user.save
-      user.create_pipedrive_lead_deal
-      render status: 200, json: user_params
-    rescue => e
-      p "p #{e.message}  -- #{e.backtrace}"
-      Rails.logger.debug "debug #{e.message}  -- #{e.backtrace}"
-      Rails.logger.info "info #{e.message}  -- #{e.backtrace}"
-      render status: 200, json: user_params.merge(failed: true, error: e.message, backtrace: e.backtrace)
-    end
+    user_params = params[:Parameters].merge!( password: User.generate_password, phone_verified: true )
+    user =  User.new( user_params )
+    user.save
+    user.create_pipedrive_lead_deal
+    render status: 200, json: user_params
   end
 
   def pipedrive
