cae319b557f611aeb6d090d5879cd9ffbe9fcb6c
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-06-21 14:59:56 +0300

    user params from fb lead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -104,10 +104,15 @@ class UsersController < ApplicationController
   end
   
   def fb_lead
-    user =  User.new( params[:Parameters] )
-    user.save
-    user.create_pipedrive_lead_deal
-    render status: 200, json: params
+    begin
+      user_params = params[:Parameters].merge!( password: User.generate_password, phone_verfied: true )
+      user =  User.new( user_params )
+      user.save
+      user.create_pipedrive_lead_deal
+      render status: 200, json: params
+    rescue => e
+      render status: 500, json: user_params
+    end
   end
 
   def pipedrive
