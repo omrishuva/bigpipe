868df7bb07bd769c007c264f3ebe47d7382caceb
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-07-13 12:03:29 +0300

    fix

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,6 @@
 class Activity < Entity
 
-	attr_accessor :id, :title, :locale, :gender, 
-  :cover_image_cloudinary_id, :about_text, :created_by, :owner
-  :created_at, :updated_at
+	attr_accessor :id, :title, :locale, :gender, :image_ids, :about_text, :created_by, :owner, :created_at, :updated_at
 
 	def self.create( params )
     self.new(params).save
