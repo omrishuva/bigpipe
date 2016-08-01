b3fd71cec2a19755adb14eb9813eb14d77c9eafc
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-08-01 17:31:49 +0300

    test

--- deploy.rb
+++ deploy.rb
@@ -33,7 +33,7 @@ class AppVersion < Entity
 
 		def create_new_version_number
 			#last_version_query = $datastore.query.kind(app_name).where( "branch", "=", branch ).order("created_at", :desc).limit(1)
-			app_version = AppVersion.last.to_i +1 #$datastore.run( last_version_query, namespace: "app_versions" )[0]["version_number"] rescue 0
+			app_version = (AppVersion.last.to_i + 1) #$datastore.run( last_version_query, namespace: "app_versions" )[0]["version_number"] rescue 0
 			new_app_version = AppVersion.new( created_at: Time.now.utc, version_number: app_version, branch: branch, developer: `whoami`  )
 			binding.pry
 			# new_app_version_entity =  $datastore.entity app_name do |e|
