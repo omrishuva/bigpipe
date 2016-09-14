0d39822a608246deb02d05669f357d374e555206
Omri Shuva <omrishuva@omris-mbp.mynet>
2016-09-14 04:11:48 +0300

    update site text on the fly

--- app/models/site_text.rb
+++ app/models/site_text.rb
@@ -1,6 +1,7 @@
 class SiteText < Entity
 	attr_accessor :id, :name, :locale, :compressed_text, :created_at, :updated_at, :created_by, :updated_by
-	
+	after_save :refresh_cache_key
+
 	def text
 		Zlib::Inflate.inflate(Base64.decode64(compressed_text)) if compressed_text.present?
 	end
@@ -12,4 +13,9 @@ class SiteText < Entity
 	def namespace
 		"super_admin"
 	end
+	
+	def refresh_cache_key
+		$wizards[generate_key] = text
+	end
+
 end
